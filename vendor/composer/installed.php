2a55ff219a03ad78ba2af702f3dcd663bbeb6b3c
Rafael <rafaeldoimo@casadocredito.com.br>
2023-03-01 17:13:35 -0300

    validar pagaemnto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rafael.doimo/bradesco_api',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f7e133e10b22b6db28208cff561ae6f0b532f8ec',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'firebase/php-jwt' => array(
+            'pretty_version' => 'v6.4.0',
+            'version' => '6.4.0.0',
+            'reference' => '4dd1e007f22a927ac77da5a3fbb067b42d3bc224',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../firebase/php-jwt',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'rafael.doimo/bradesco_api' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f7e133e10b22b6db28208cff561ae6f0b532f8ec',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
